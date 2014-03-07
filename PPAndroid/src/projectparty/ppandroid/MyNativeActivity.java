71412d3e98170dd9ebc5004d413585b7f4a21945
ponpal <pontus@pontuspall.com>
2014-03-07 21:52:56 +0100

    Keeping the screen on in NativeActivity

--- PPAndroid/src/projectparty/ppandroid/MyNativeActivity.java
+++ PPAndroid/src/projectparty/ppandroid/MyNativeActivity.java
@@ -1,8 +1,16 @@
 package projectparty.ppandroid;
 
+import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MyNativeActivity extends android.app.NativeActivity 
 {
+	@Override
+	protected void onCreate(Bundle savedInstance) {
+		super.onCreate(savedInstance);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+	}
+	
 	static 
 	{
 		System.loadLibrary("mystery");
