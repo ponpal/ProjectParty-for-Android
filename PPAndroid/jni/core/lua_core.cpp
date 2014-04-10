6ede2b04f6821eb4d6d4c7ac49f7fae342b0f20f
ponpal <pontus@pontuspall.com>
2014-04-10 16:00:39 +0200

    Error logging is better now

--- PPAndroid/jni/core/lua_core.cpp
+++ PPAndroid/jni/core/lua_core.cpp
@@ -147,7 +147,7 @@ void loadLuaScript(const std::string& pathInFiles)
 	error |= lua_pcall(luaState, 0, 0, 0);
 
 	if(error)
-		LOGE("LUA SCRIPT ERROR %s", lua_tostring(luaState, -1));
+		LOGE("LUA SCRIPT ERROR %s in file %s", lua_tostring(luaState, -1), pathInFiles.c_str());
 }
 
 void runLuaGarbageCollector(int milisecs)
